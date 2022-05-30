9dcef535e23c87cf84d82aeb1440bacd3bd03536
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-05-30 14:01:40 +0100

    edited

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // activity 1
 
+const runFive = (str) => {
+    for (i = 0; i < 5; i++) {
+    console.log(str);
+}
+};
+const runOnce = (str, fn) => {
+    fn(str);
+};
+
+runOnce("Hello Codenation", runFive);
+
+
+
+
+
