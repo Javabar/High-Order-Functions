17a10d425194992936158e8157d1012b44eb76f0
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-06-01 15:05:56 +0100

    edited

--- app.js
+++ app.js
@@ -11,6 +11,41 @@ const runOnce = (str, fn) => {
 
 runOnce("Hello Codenation", runFive);
 
+// activity 2
+
+let arrNum = [2, 5, 10, 20, 400];
+
+const multiThree = (input) => {
+    return input.map(x => x * 3);
+};
+
+
+// Activity 3
+
+const add = (a, b) => {
+    return a + b;
+};
+const subtract = (a, b) => {
+    return a -b;
+};
+const multiply = (a, b) => {
+    return a * b;
+};
+const divide = (a, b) => {
+    return a / b;
+};
+
+const doMaths = (num1) => {
+    return (num2, fn) => {
+        return fn(num1, num2);
+    }
+};
+
+console.log(doMaths(12));
+console.log(doMaths(12)(10, add))
+console.log(doMaths(12)(10, divide))
+console.log(doMaths(12)(10, multiply))
+console.log(doMaths(12)(10, subtract))
 
 
 
